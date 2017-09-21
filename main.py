9c4847b0eb12d662b3387e55627fce7324421297
paul <paulvrobson@gmail.com>
2017-09-21 16:11:39 -0500

    moved the cgi.escape underneath the todo

--- main.py
+++ main.py
@@ -91,7 +91,7 @@ def add_movie():
 
     # TODO 
     # 'escape' the user's input so that if they typed HTML, it doesn't mess up our site
-    
+    new_movie = cgi.escape(new_movie)
     # TODO 
     # if the user typed nothing at all, redirect and tell them the error
     if not new_movie:
@@ -105,7 +105,7 @@ def add_movie():
         return redirect("/?error={0}".format(bad_movie))
 
     # build response content
-    new_movie_element = "<strong>" + cgi.escape(new_movie) + "</strong>"
+    new_movie_element = "<strong>" + new_movie + "</strong>"
     sentence = new_movie_element + " has been added to your Watchlist!"
     content = page_header + "<p>" + sentence + "</p>" + page_footer
 
